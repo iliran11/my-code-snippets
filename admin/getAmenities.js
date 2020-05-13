44fd4bee6c53a05863f6b5f98c9be919a31ee39f
Liran Cohen <iliran11@gmail.com>
2020-05-13 12:51:39 +0300

    get amenities

---
+++ admin/getAmenities.js
@@ -0,0 +1,14 @@
+// works on this page: https://admin.bookaway.com/key-detail-definitions
+// GOAL: make a i18next json from the amenities.
+const json = {};
+fetch("https://cdn.jsdelivr.net/npm/lodash@4.17.4/lodash.min.js")
+  .then((response) => response.text())
+  .then((text) => eval(text));
+let tbodyElem = document.getElementsByTagName("tbody")[0];
+for (let row of tbodyElem.children) {
+  const amenity = row.children[1].innerHTML;
+  json[_.snakeCase(amenity)] = amenity;
+}
+
+// print the json with the keys
+console.log(json);
